a986317e2e3ceff42641cae9df8f6341fd5e2390
shifeng3 <shifeng3@staff.sina.com.cn>
2016-09-14 14:20:52 +0800

    add demo02.php

---
+++ demo01.php
@@ -0,0 +1,15 @@
+<?php
+//测试文件
+error_reporting(E_ALL & ~E_NOTICE);
+
+$arr = array();
+$arr1 = array(
+  array('fund_code'=>1,'name'=>'哈哈'),
+  array('fund_code'=>2,'name'=>'大大')
+);
+
+array_walk($arr1,function($val) use(&$arr){
+  $arr[] = intval($val['fund_code']);
+});
+
+print_r($arr);
